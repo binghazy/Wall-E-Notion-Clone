3c3c5fac7d31eef8c8a4a4dd20d11b80803c427d
Ahmed Ghazy <ahmedghaziwork@gmail.com>
2026-04-10 19:51:16 +0200

    version 1.0.0b fixed errors again

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   images: {
     domains: ["files.edgestore.dev"],
   },
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings ?? []),
+      /autoprefixer: start value has mixed support, consider using flex-start instead/,
+    ];
+    config.infrastructureLogging = {
+      ...(config.infrastructureLogging ?? {}),
+      level: "error",
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
